28d03d6cef640b0f57e327144c61a4d4edebd46b
Leon Stier <Lms@gnadenweiler.de>
2022-12-31 17:05:37 +0100

    Added BPM slider

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ const note_display = document.querySelector(".note-display");
 const note_buttons = document.getElementsByClassName("note-button");
 const note_selection = document.querySelector(".note-selection");
 const note_subscript = document.querySelector(".note-subscript");
+const error_div = document.querySelector(".error-message");
+const input = document.querySelector("#bpm");
+const value = document.querySelector("#bpm-label");
+const toggle_switch = document.querySelector("#switch-input");
+const switch_position = document.querySelector("#switch-position");
+var slider_mode = "BPM";
+
+
+input.addEventListener("input", (event) => {
+	value.textContent = slider_mode + ": " + event.target.value
+	Tone.Transport.bpm.value = input.value;
+})
+
+toggle_switch.addEventListener("input", (event) => {
+	if (toggle_switch.checked) {
+		switch_position.innerHTML = "BPM"
+		slider_mode = "BPM";
+	} else {
+		switch_position.innerHTML = "BPM"
+	}
+})
+
 const synth = new Tone.Synth().toDestination();
 var stopped = true;
 
 for (const button of note_buttons) {
 	button.addEventListener("click", function() {
 		button.classList.toggle("note-button__selected");
+		error_div.style.visibility = "hidden";
 	});
 }
 
@@ -35,6 +58,8 @@ function notePlayer(random) {
 	let selected_buttons = document.getElementsByClassName("note-button__selected");
 	if (selected_buttons.length == 0) {
 		stopped = true;
+		error_div.innerHTML = "You need to select at least one note";
+		error_div.style.visibility = "visible";
 		return;
 	}
 
@@ -47,12 +72,18 @@ function notePlayer(random) {
 		+ selected_buttons[index].dataset.octave.sub();
 		synth.triggerAttackRelease(selected_buttons[index].textContent + selected_buttons[index].dataset.octave, "1n", time);
 		if (random) {
-			index = Math.floor(Math.random() * selected_buttons.length);
+			prev_index = index;
+			while (prev_index == index) {
+				index = Math.floor(Math.random() * selected_buttons.length);
+			}
 		} else {
 			index = (index + 1) % selected_buttons.length;
 		}
 	}, "1n").start(0);
-	Tone.Transport.bpm.value = 100;
+	if (slider_mode === "Seconds") {
+		Tone.Transport.bpm.value = input.value;
+
+	}
 	Tone.Transport.start();
 }
 
